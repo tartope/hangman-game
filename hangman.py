6dfc3443a3c6c63b537b01ee12bbe584e69a5ade
Tunisia Artope <tartope@xpanxion.com>
2023-06-27 20:33:32 -0400

    Add number of tries. Partially working.

--- hangman.py
+++ hangman.py
@@ -49,12 +49,18 @@ def num_of_tries(attempts):
 
 
 def game_board(guess): 
-               
+    num_tries = 7
+    if num_tries == 0: 
+        print("Game over") 
+        return
     if guess in word:
-        print("Good guess")
-        print(HANGMAN_PICS[0]) 
-        another_guess = input("Guess again: ")
-        game_board(another_guess)
+        while num_tries > 1 and num_tries <=7:
+            print("Good guess")
+            print(HANGMAN_PICS[0]) 
+            num_tries -= 1
+            print(f"Tries left: {num_tries}")
+            another_guess = input("Guess again: ")
+            # game_board(another_guess)
     # else:
     #     print("Oops! That is not correct.")
 
